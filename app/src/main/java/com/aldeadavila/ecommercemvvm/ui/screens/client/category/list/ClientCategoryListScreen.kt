8a48956fc0c53e6257ba9cb374db021ca04c3df3
Francisco Jose Marquina Garcia <aldeadavila@gmail.com>
2023-08-28 22:00:31 +0200

    77 listar categorías cliente

--- app/src/main/java/com/aldeadavila/ecommercemvvm/ui/screens/client/category/list/ClientCategoryListScreen.kt
+++ app/src/main/java/com/aldeadavila/ecommercemvvm/ui/screens/client/category/list/ClientCategoryListScreen.kt
@@ -3,12 +3,14 @@ package com.aldeadavila.ecommercemvvm.ui.screens.client.category.list
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import com.aldeadavila.ecommercemvvm.ui.screens.client.category.list.components.GetCategories
 import com.aldeadavila.ecommercemvvm.ui.screens.profile.components.ClientCategoryListContent
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ClientCategoryListScreen() {
+fun ClientCategoryListScreen(navHostController: NavHostController,) {
     Scaffold {
-        ClientCategoryListContent(paddingValues = it)
+        GetCategories(paddingValues = it, navHostController = navHostController )
     }
 }
